503ed20cb9ca51f0adc7da66ac89bf1f67d13861
BEWCHAN <bewchan06@gmail.com>
2023-10-27 14:04:07 +0700

    chuyển giao diện giữa trả hàng và bán hàng  - thêm chuyên giao diện sản phẩm

--- POS SHOP/src/gui/QuanLySanPham.java
+++ POS SHOP/src/gui/QuanLySanPham.java
@@ -1,9 +1,9 @@
 package gui;
 
-import java.awt.CardLayout;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+import java.awt.CardLayout;
 import javax.swing.JLabel;
 
 public class QuanLySanPham extends JPanel {
@@ -12,12 +12,11 @@ public class QuanLySanPham extends JPanel {
 	 * Create the panel.
 	 */
 	public QuanLySanPham() {
-		setLayout(new CardLayout(0, 0));
-
-		// Đặt kích thước mặc định cho JPanel QuanLySanPham
 		setPreferredSize(new Dimension(947, 664));
+		setLayout(new CardLayout(0, 0));
 		
-		JLabel lblNewLabel = new JLabel("quan ly san pham");
-		add(lblNewLabel, "name_309818123584300");
+		JLabel lblNewLabel = new JLabel("quản lý sản phẩm");
+		add(lblNewLabel, "name_45624352660600");
 	}
+
 }
